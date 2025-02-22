a2896323027d756032bcd93b2982b516d98c48c2
Aaoligei <253426887@qq.com>
2025-02-22 21:56:14 +0800

    引擎核心部分

---
+++ mini2D game Engine/core/Engine.cpp
@@ -0,0 +1,55 @@
+#include "Engine.h"
+#include<iostream>
+
+Engine* Engine::s_Instance = nullptr;
+
+bool Engine::Init()
+{
+	if (SDL_Init(SDL_INIT_VIDEO) != 0 && IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG) != 0) {
+		SDL_Log("Failed to initialize SDL: %s", SDL_GetError());
+		return false;
+	}
+
+	m_Window = SDL_CreateWindow("Soft Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+	if (m_Window == nullptr) {
+		SDL_Log("Failed to create window: %s", SDL_GetError());
+		return false;
+	}
+
+	m_Renderer = SDL_CreateRenderer(m_Window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (m_Renderer == nullptr) {
+		SDL_Log("Failed to create renderer: %s", SDL_GetError());
+		return false;
+	}
+	return m_IsRunning = true;
+}
+
+void Engine::Clean()
+{
+}
+
+void Engine::Quit()
+{
+	m_IsRunning = false;
+}
+
+void Engine::Update()
+{
+	std::cout << "Updating..." << std::endl;
+}
+
+void Engine::Render()
+{
+	SDL_SetRenderDrawColor(m_Renderer, 100, 100, 0, 255);
+	SDL_RenderClear(m_Renderer);
+	SDL_RenderPresent(m_Renderer);
+}
+
+void Engine::Events()
+{
+	SDL_Event event;
+	SDL_PollEvent(&event);
+	switch (event.type) {
+	case SDL_QUIT:Quit();
+	}
+}
---
+++ mini2D game Engine/main.cpp
@@ -0,0 +1,16 @@
+#include"core/Engine.h"
+
+#define SDL_MAIN_HANDLED
+
+int main(int argc,char* argv[]) {
+	
+	Engine::GetInstance()->Init();
+	while (Engine::GetInstance()->IsRunning()) {
+		Engine::GetInstance()->Events();
+		Engine::GetInstance()->Update();
+		Engine::GetInstance()->Render();
+	}
+
+	Engine::GetInstance()->Clean();
+	return 0;
+}
