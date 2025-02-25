24ef3e1bec5f783ab72f470e333ee2ae132a1e83
Aaoligei <253426887@qq.com>
2025-02-25 18:54:58 +0800

    向量和gameobject

---
+++ mini2D game Engine/Physics/Transform.h
@@ -0,0 +1,15 @@
+#pragma once
+#include"Vector2D.h"
+class Transform
+{
+public:
+	float X, Y;
+
+	Transform(float x=0,float y=0):X(x),Y(y){}
+
+private:
+	inline void TranslateX(float x) { X += x; }
+	inline void TranslateY(float y) { Y += y; }
+	inline void Translate(Vector2D v) { X += v.X; Y += v.Y; }
+};
+
